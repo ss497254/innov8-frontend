0a9c93706963bdc79d679735e178b0604713684a
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-05-21 23:58:44 +0530

    feat: add more icons

--- packages/common/src/icons/index.ts
+++ packages/common/src/icons/index.ts
@@ -1,2 +1,7 @@
-export * from "./LogoIcon";
 export * from "./GoogleIcon";
+export * from "./LogoIcon";
+
+export * from "./BugIcon";
+export * from "./HamburgerIcon";
+export * from "./BackIcon";
+export * from "./CaretRightIcon";
