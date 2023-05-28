9cc1708e3911d90394bad0251b41c4815ad049c1
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-05-29 00:58:15 +0530

    update: judge project review page

--- apps/judge/src/pages/projects/[projectId]/review-project.tsx
+++ apps/judge/src/pages/projects/[projectId]/review-project.tsx
@@ -1,17 +1,25 @@
-import { NextPageWithLayout } from "common";
+import { NextPageWithLayout, ProjectType, ResponseType } from "common";
 import { Button, Input, StarRating, Textarea } from "common/src/ui";
+import { useRouter } from "next/router";
 import { useState } from "react";
 import { AuthenticatedRoute } from "src/components/AuthenticatedRoute";
+import useSWRImmutable from "swr/immutable";
 
 const ReviewProject: NextPageWithLayout = () => {
   const [value, setValue] = useState(0);
+  const { query } = useRouter();
+
+  const { data: res, isLoading } = useSWRImmutable<ResponseType<ProjectType>>(
+    query.id && `/employee/projects/${query.id}`
+  );
+
   return (
     <div className="max-w-6xl rounded-md mx-auto min-h-full p-4 md:p-6 lg:p-8">
       <div className="bg-white rounded-md shadow-xl p-6 md:p-8 space-y-6">
         <h3>Project Review</h3>
         <Input
           disabled
-          value="Illustration of onboarding"
+          value={res?.data.name}
           label="Project Name"
           labelClassName="md:text-lg"
         />
@@ -19,31 +27,36 @@ const ReviewProject: NextPageWithLayout = () => {
           disabled
           label="Elevator pitch"
           labelClassName="md:text-lg"
-          value="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam quis, magnam nihil modi accusamus aliquid ipsam eum impedit delectus cumque dolore officia, odit odio minus nobis voluptatum nisi vero porro"
+          rows={4}
+          value={res?.data.elevatorPitch}
         />
         <Textarea
           disabled
           label="Summary"
           labelClassName="md:text-lg"
-          value="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam quis, magnam nihil modi accusamus aliquid ipsam eum impedit delectus cumque dolore officia, odit odio minus nobis voluptatum nisi vero porro"
+          rows={4}
+          value={res?.data.summary}
         />
         <Textarea
           disabled
           label="How will you capture value?"
           labelClassName="md:text-lg"
-          value="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam quis, magnam nihil modi accusamus aliquid ipsam eum impedit delectus cumque dolore officia, odit odio minus nobis voluptatum nisi vero porro"
+          rows={4}
+          value={res?.data.teamOverview}
         />
         <Textarea
           disabled
           label="Do you have the competencies within your team to build the MVP?"
           labelClassName="md:text-lg"
-          value="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam quis, magnam nihil modi accusamus aliquid ipsam eum impedit delectus cumque dolore officia, odit odio minus nobis voluptatum nisi vero porro"
+          rows={4}
+          value={res?.data.teamOverview}
         />
         <Textarea
           disabled
           label="Please edit and finalize your pitch deck using the template"
           labelClassName="md:text-lg"
-          value="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam quis, magnam nihil modi accusamus aliquid ipsam eum impedit delectus cumque dolore officia, odit odio minus nobis voluptatum nisi vero porro"
+          rows={4}
+          value={res?.data.files?.join(", ")}
         />
         <div className="space-y-2">
           <div className="md:text-lg font-medium text-gray-900">
