61190eb903a3a6bd2fb422822d2f05a712c2faad
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-06-13 20:25:23 +0530

    feat: add project-form page

--- apps/super-admin/src/components/AuthenticatedRoute.tsx
+++ apps/super-admin/src/components/AuthenticatedRoute.tsx
@@ -1,8 +1,9 @@
 import { AppLayout, ProtectedRoute } from "common/src/components";
 import {
   DashboardIcon,
-  EditIcon,
+  PaperClipIcon,
   ProfileIcon,
+  ProjectIcon,
   SettingsIcon,
 } from "common/src/icons";
 import React from "react";
@@ -13,9 +14,14 @@ export const items = [
     items: [
       { href: "/", icon: <DashboardIcon size={22} />, title: "Dashboard" },
       {
-        href: "/notifications",
-        icon: <EditIcon size={24} />,
-        title: "My Tasks",
+        href: "/project-form",
+        icon: <ProjectIcon size={24} />,
+        title: "Project Form",
+      },
+      {
+        href: "/users",
+        icon: <PaperClipIcon size={22} />,
+        title: "Users",
       },
       { href: "/profile", icon: <ProfileIcon size={24} />, title: "Profile" },
       {
---
+++ apps/super-admin/src/pages/project-form/index.tsx
@@ -0,0 +1,27 @@
+import { NextPageWithLayout, PageTopBar, TabButtons } from "common";
+import { useState } from "react";
+import { AuthenticatedRoute } from "src/components/AuthenticatedRoute";
+
+let tabs = ["View", "Edit"];
+
+const ProjectForm: NextPageWithLayout = () => {
+  const [activeTab, setActiveTab] = useState(tabs[0]);
+
+  return (
+    <div className="max-w-7xl mx-auto min-h-full p-4 md:p-6">
+      <PageTopBar heading="Dashboard">
+        <TabButtons
+          tabs={tabs}
+          activeTab={activeTab}
+          setActiveTab={setActiveTab}
+        />
+      </PageTopBar>
+    </div>
+  );
+};
+
+ProjectForm.getLayout = (page) => (
+  <AuthenticatedRoute>{page}</AuthenticatedRoute>
+);
+
+export default ProjectForm;
